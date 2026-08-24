ad7c2387127260a13e0727f63ee6764371d5e340
Ankit Adhikari <ankitadhikaripro@gmail.com>
2026-08-24 23:16:37 +0530

    hello

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 const port =process.env.port ?? 8080;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Hello, Ankits !");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is  fucking running on http://localhost:${port}`);
 });
